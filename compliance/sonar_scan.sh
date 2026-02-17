2f8070ae529af41b1bb8775efd847e42d38d6f06
Joshua Quek <joshua.quek.fullstack@gmail.com>
2026-02-18 00:35:10 +0800

    feat: add auto-tune options for transfer and migrate scripts; remove high-memory and fast migrate scripts; update package dependencies

--- compliance/sonar_scan.sh
+++ compliance/sonar_scan.sh
@@ -5,6 +5,7 @@ npx sonar-scanner \
   -Dsonar.projectName="CloudVoyager" \
   -Dsonar.host.url=http://localhost:9000 \
   -Dsonar.sources=. \
+  -Dsonar.exclusions=**/node_modules/**,**/dist/**,**/build/**,**/.next/**,**/.vercel/**,**/.vscode/**,**/.idea/**,**/.git/** \
   -Dsonar.token="${SONAR_TOKEN:?Set SONAR_TOKEN environment variable}" \
   -Dsonar.branch.name=main \
   -Dsonar.javaOpts=-Xmx8192m \
