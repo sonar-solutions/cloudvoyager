658dca512f9defd8ea263a8e6a727dd4fb9b060e
Joe Tingsanchali <joe.tingsanchali@sonarsource.com>
2026-04-17 19:21:17 -0500

    Issue #94 - implement fetch with slicing logic using date slicng for getting >10K issues

--- src/pipelines/sq-9.9/sonarcloud/api/issues/helpers/search-issues.js
+++ src/pipelines/sq-9.9/sonarcloud/api/issues/helpers/search-issues.js
@@ -1,4 +1,5 @@
 import logger from '../../../../../../shared/utils/logger.js';
+import { fetchWithSlicing } from '../../../../../../shared/utils/search-slicer/index.js';
 
 // -------- Search Issues with Pagination --------
 
@@ -7,27 +8,33 @@ const ALL_STATUSES = 'OPEN,CONFIRMED,REOPENED,RESOLVED,CLOSED';
 export async function searchIssues(client, organization, projectKey, filters = {}) {
   logger.debug(`Searching issues in project: ${projectKey}`);
 
-  let allResults = [];
-  let page = 1;
-  const pageSize = 500;
+  const baseParams = {
+    componentKeys: projectKey,
+    organization,
+    statuses: ALL_STATUSES,
+    ...filters,
+  };
 
-  // eslint-disable-next-line no-constant-condition
-  while (true) {
-    const response = await client.get('/api/issues/search', {
-      params: {
-        componentKeys: projectKey, organization,
-        statuses: ALL_STATUSES, ps: pageSize, p: page,
-        ...filters
-      }
-    });
+  const probeTotalFn = async (endpoint, params) => {
+    const response = await client.get(endpoint, { params: { ...params, ps: 1, p: 1 } });
+    return response.data.paging?.total ?? 0;
+  };
 
-    const issues = response.data.issues || [];
-    allResults = allResults.concat(issues);
+  const getPaginatedFn = async (endpoint, params, dataKey) => {
+    let allResults = [];
+    let page = 1;
+    const pageSize = 500;
+    // eslint-disable-next-line no-constant-condition
+    while (true) {
+      const response = await client.get(endpoint, { params: { ...params, ps: pageSize, p: page } });
+      const items = response.data[dataKey] || [];
+      allResults = allResults.concat(items);
+      const total = response.data.paging?.total || 0;
+      if (page * pageSize >= total || items.length < pageSize) break;
+      page++;
+    }
+    return allResults;
+  };
 
-    const total = response.data.paging?.total || 0;
-    if (page * pageSize >= total || issues.length < pageSize) break;
-    page++;
-  }
-
-  return allResults;
+  return await fetchWithSlicing(probeTotalFn, getPaginatedFn, '/api/issues/search', baseParams, 'issues');
 }
